f337b41537828b27eb87ea95b91416d6f14c6e20
mmiikeke <o860116@gmail.com>
2020-08-09 22:57:56 +0800

    GUI setting page

--- user_interface/main_window.py
+++ user_interface/main_window.py
@@ -24,7 +24,7 @@ from PySide2.QtCore import Qt, QFile, QPropertyAnimation
 from PySide2.QtUiTools import QUiLoader
 from PySide2.QtGui import QFont, QIcon, QPixmap
 
-from user_interface.page_widget import page1
+from user_interface.page_widget import page1, page2
 
 __copyright__ = 'Copyright © 2020 mmiikeke - All Right Reserved.'
 
@@ -48,6 +48,10 @@ class MainWindow(object):
         self._pages = OrderedDict()
         self.setup_ui()
 
+        #self._window.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        #self._window.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        #self.ui.frame_label_top_btns.mouseDoubleClickEvent = dobleClickMaximizeRestore
+
     @property
     def window(self):
         """The main window object"""
@@ -62,26 +66,103 @@ class MainWindow(object):
         file.close()
 
         self._pages['page1'] = page1()
+        self._pages['page2'] = page2()
 
         for index, name in enumerate(self._pages):
             print('pages {} : {} page'.format(index, name))
             self._window.stackedWidget.addWidget(self._pages[name].widget)
         
+        self._window.stackedWidget.setCurrentIndex(0)
+
         self.set_buttons()
+
+        #self._window.stackedWidget.setLayout(g_layout)
+        #self._window.frame_content.addWidget(self._pages[name].widget)
+
+        #self._window.widget_stack.setCurrentIndex(0)
+
+        # Build up signal / slot
+        #self._option_panel.currentItemChanged.connect(self.set_page)
 
     def set_buttons(self):
         """Setup buttons"""
-        self._pages['page1'].widget.btn_start.clicked.connect(self.page1_start)
+        self._pages['page1'].widget.btn_start.clicked.connect(lambda: self.next_page(1))
+
+        #self._window.btn_page1_start.clicked.connect(self.page1_start)
+        #self._window.import_btn.setIcon(QIcon('basic_widget\\media\\import.svg'))
+        #self._window.btn_toggle.clicked.connect(lambda: self.toggleMenu(250, True))
+
+        #self._window.btn_menu_1.clicked.connect(lambda: self.set_page(0))
+        #self._window.btn_menu_2.clicked.connect(lambda: self.set_page(1))
+        #self._window.btn_menu_3.clicked.connect(lambda: self.set_page(2))
+        
+        #p = QPixmap(IMAGE);
+        #self._window.label_page1_bg.setPixmap(p)
+        #self._window.label_page1_bg.setMask(p.mask());
+        #w = self._window.frame_content.width();
+        #h = self._window.frame_content.height();
+        #print(f'w={w}, h={h}')
+        # set a scaled pixmap to a w x h window keeping its aspect ratio 
+        #self._window.label_page1_bg.setPixmap(p.scaled(1500,1200,QtCore.Qt.KeepAspectRatio));
 
     @QtCore.Slot()
-    def page1_start(self):
-        print('test')
-        print(self._pages['page1'].widget.geometry())
+    def next_page(self, page):
+        
+        current_widget = self._window.stackedWidget.currentWidget()
+        print(current_widget.geometry())
 
         # ANIMATION
-        self.animation = QPropertyAnimation(self._pages['page1'].widget, b"geometry")
-        self.animation.setDuration(1000)
-        self.animation.setStartValue(self._pages['page1'].widget.geometry())
-        self.animation.setEndValue(QtCore.QRect(0, 500, 1000, 435))
+        self.animation = QPropertyAnimation(current_widget, b"geometry")
+        self.animation.setDuration(2000)
+        self.animation.setStartValue(current_widget.geometry())
+        self.animation.setEndValue(QtCore.QRect(-1000, 0, 1000, 435))
         self.animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
         self.animation.start()
+
+        QtCore.QTimer.singleShot(2000, lambda: self.set_page(1))
+
+    @QtCore.Slot()
+    def set_page(self, page):
+        """Slot, switch page of stack widget"""
+        self._window.stackedWidget.setCurrentIndex(page)
+
+                # ANIMATION
+        self.animation = QPropertyAnimation(self._window.stackedWidget.currentWidget(), b"geometry")
+        self.animation.setDuration(2000)
+        self.animation.setStartValue(QtCore.QRect(1000, 0, 1000, 435))
+        self.animation.setEndValue(QtCore.QRect(0, 0, 1000, 435))
+        self.animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+        self.animation.start()
+
+    @QtCore.Slot()
+    def toggleMenu(self, maxWidth, enable):
+        print('start')
+        if enable:
+
+            # GET WIDTH
+            width = self._window.frame_left_menu.width()
+            print(f'width = {width}')
+            maxExtend = maxWidth
+            standard = 70
+
+            # SET MAX WIDTH
+            if width == 70:
+                widthExtended = maxExtend
+            else:
+                widthExtended = standard
+
+            # ANIMATION
+            self.animation = QPropertyAnimation(self._window.frame_left_menu, b"minimumWidth")
+            self.animation.setDuration(400)
+            self.animation.setStartValue(width)
+            self.animation.setEndValue(widthExtended)
+            self.animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+            self.animation.start()
+    
+    @QtCore.Slot(str)
+    def say_hello(self, msg):
+        print('Hello ' + msg)
+
+    @QtCore.Slot()
+    def exit(self):
+        self._window.close()
--- user_interface/page_widget.py
+++ user_interface/page_widget.py
@@ -20,10 +20,12 @@ from PySide2 import QtCore
 from PySide2.QtCore import Qt, QFile, QObject, QPropertyAnimation
 from PySide2.QtUiTools import QUiLoader
 from PySide2.QtGui import QFont, QIcon
+from PySide2.QtWidgets import QFileDialog
 
 __copyright__ = 'Copyright © 2020 mmiikeke - All Right Reserved.'
 
 PAGE1 = 'user_interface/form/ui_page1.ui'
+PAGE2 = 'user_interface/form/ui_page2.ui'
 
 class page1(QObject):
     def __init__(self, parent=None):
@@ -50,3 +52,46 @@ class page1(QObject):
 
     def set_buttons(self):
         """Setup buttons"""
+
+class page2(QObject):
+    def __init__(self, parent=None):
+
+        super(page2, self).__init__(parent)
+
+        self._widget = None
+
+        self.setup_ui()
+        
+    @property
+    def widget(self):
+        return self._widget
+    
+    def setup_ui(self):
+        """Initialize user interface of widget."""
+        loader = QUiLoader()
+        file = QFile(PAGE2)
+        file.open(QFile.ReadOnly)
+        self._widget = loader.load(file)
+        file.close()
+
+        self.set_buttons()
+
+    def set_buttons(self):
+        """Setup buttons"""
+        self._widget.input_button.clicked.connect(self.select_input)
+        self._widget.output_button.clicked.connect(self.select_output)
+
+    @QtCore.Slot()
+    def select_input(self):
+        if self._widget.set_video.isChecked():
+            file = str(QFileDialog.getOpenFileName(None, "Select Video", "./", "MP4 File (*.mp4)")[0])
+        else:
+            file = str(QFileDialog.getExistingDirectory(None, "Select Directory"))
+
+        self._widget.lineEdit_input.setText(file)
+    
+    @QtCore.Slot()
+    def select_output(self):
+        file = str(QFileDialog.getExistingDirectory(None, "Select Directory"))
+
+        self._widget.lineEdit_output.setText(file)
